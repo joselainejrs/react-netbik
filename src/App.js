04619d442b1bbc4e37398d69173d2c8b61676580
Joselaine Romao Soares <2300061@SALAS.AULAS>
2024-09-06 22:00:14 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <img src="../img/ravem.webp" alt="Ravem" className="img" />
+      <form>
+        <fildeset>
+          <label>
+            Nome:
+            <input type="text" name="name" />
+          </label>
+          
+          <select>
+            <option value="laranja">Laranja</option>
+            <option value="limao">Limão</option>
+            <option selected value="coco">Coco</option>
+            <option value="manga">Manga</option>
+          </select>
+
+          <input type="submit" value="Enviar" />
+        </fildeset>
+      </form>
+
+
     </div>
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       hola
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 
